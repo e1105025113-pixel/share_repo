d68413cad485b9c995b6b336ac9ef5cb9c459b76
yuchel <e1105025113@oit.ac.jp>
2026-09-01 20:42:21 +0900

    4-wheel

--- build/gakurobo/prefix_override/sitecustomize.py
+++ build/gakurobo/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/yuchel/shared_repo/install/gakurobo'
+    sys.prefix = sys.exec_prefix = '/home/yuchel/share_repo/install/gakurobo'
--- build/test_pkg/prefix_override/sitecustomize.py
+++ build/test_pkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/yuchel/shared_repo/install/test_pkg'
+    sys.prefix = sys.exec_prefix = '/home/yuchel/share_repo/install/test_pkg'
